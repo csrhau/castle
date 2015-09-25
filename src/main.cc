a0a8e28c80259f9ad7625566e135de1a5d550ff8
Stephen Roberts <stephenianroberts@gmail.com>
2015-09-25 18:14:56 +0100

    Donesie

--- src/main.cc
+++ src/main.cc
@@ -1,92 +1,85 @@
 #include <iostream>
 #include <algorithm>
 
+#include <stdlib.h>
+#include<cilk/cilk.h>
+
 #include "grid.h"
 #include "castle_config.h"
 
-
-#define SIM_ROWS 31
-#define SIM_COLS 31
-#define STENCIL_RADIUS 1 
-#define TIME_STEPS 10
-
 int main(int argc, char *argv[]) {
-  std::cout << "Castle " << CASTLE_VERSION_MAJOR << CASTLE_VERSION_MINOR << std::endl;
+  std::cout << "Castle v" << CASTLE_VERSION_MAJOR << CASTLE_VERSION_MINOR 
+            << "\nSimulating a " << NX << "x" << NY << " domain." << std::endl; 
 
-  double **u0 = new double*[SIM_ROWS];
-  double **u1 = new double*[SIM_ROWS];
+  // Define values
+  const size_t nx = NX;
+  const size_t ny = NY;
+  const size_t nt = NT;
+  const double nu = NU;
+  const double sigma = SG;
+  const double dx = XD/(nx - 1.0);
+  const double dy = YD/(ny - 1.0);
+  const double dt = sigma * dx * dy / nu;
 
-  // Array notation doesn't work for this allocation
-  for (int r = 0; r < SIM_ROWS; ++r) {
-    u0[r] = new double[SIM_COLS];
-    u1[r] = new double[SIM_COLS];
+  // Allocate space for simulation
+  double **u0 = new double*[ny];
+  double **u1 = new double*[ny];
+  for (int r = 0; r < ny; ++r) {
+    u0[r] = new double[nx];
+    u1[r] = new double[nx];
   }
 
-  std::cout << u0[0] << std::endl;
-  std::cout << u0[1] << std::endl;
-  std::cout << u0[2] << std::endl;
+  // Initialize space
+  u0[0:ny][0:nx] = 1.0;
+  u0[ny/4:ny/2][nx/4:nx/2] = 2.0;
 
-  size_t row_y = STENCIL_RADIUS;
-  size_t rows = SIM_ROWS - 2*STENCIL_RADIUS;
-  size_t col_x = STENCIL_RADIUS;
-  size_t cols = SIM_COLS - 2*STENCIL_RADIUS;
+  // Calculate initial temperature
+  const double tInit = __sec_reduce_add(u0[1:ny-2][1:nx-2*1]);
+  std::cout << "Initial temperature: " << tInit << std::endl;
 
+  const double cx = nu * dt / (dx * dx);
+  const double cy = nu * dt / (dy * dy); 
 
+  for (size_t t = 0; t < nt; ++t) {
+#if defined(CILK_PAR)
+    std::cout << "par" << std::endl;
+    cilk_for (size_t r = 1; r < nx - 1; ++r) {
+      u1[r][1:nx-2] = u0[r][1:nx-2] 
+                                 + cx * (u0[r][0:nx-2] 
+                                      - 2 * u0[r][1:nx-2] 
+                                      + u0[r][2:nx-2])
+                                 + cy * (u0[r-1][1:nx-2] 
+                                      - 2 * u0[r][1:nx-2] 
+                                      + u0[r+1][1*1:nx-2]);
+    }
+#else
+    std::cout << "cereal" << std::endl;
+    u1[1:ny-2][1:nx-2] = u0[1:ny-2][1:nx-2] 
+                               + cx * (u0[1:ny-2][0:nx-2] 
+                                    - 2 * u0[1:ny-2][1:nx-2] 
+                                    + u0[1:ny-2][2:nx-2])
+                               + cy * (u0[0:ny-2][1:nx-2] 
+                                    - 2 * u0[1:ny-2][1:nx-2] 
+                                    + u0[2:ny-2][1*1:nx-2]);
+#endif
+    // Reflect Boundaries TODO: consider stencil radius
+    u1[0][0:nx] = u1[1][0:nx];
+    u1[ny-1][0:nx] = u1[ny-2][0:nx];
+    u1[0:ny][0] = u1[0:ny][1];
+    u1[0:ny][nx-1] = u1[0:ny][nx-2];
 
-  // Initialize
-  u0[0:SIM_ROWS][0:SIM_COLS] = 1.0;
-  u1[0:SIM_ROWS][0:SIM_COLS] = 1.0;
-
-
-  double vis = 1;
-  double dx = 2.0 / (SIM_COLS-1);
-  double dy = 2.0 / (SIM_ROWS-1);
-  double nu = 0.05;
-  double sigma = 0.25;
-  double dt = sigma * dx * dy / nu;
-
-  double cx = vis*dt/(dx*dx);
-  double cy = vis*dt/(dy*dy);
-
-  double tNext, tNow = __sec_reduce_add(u0[0:SIM_ROWS][0:SIM_COLS]);
-  std::cout << "Starting..." << std::endl;
-  for (size_t ts = 0; ts < TIME_STEPS; ++ts) {
-    // Diffusion Equation
-    u1[row_y:rows][col_x:cols] = u0[row_y:rows][col_x:cols]       // x,y 
-                               + cx*u0[row_y:rows][col_x-1:cols]  // x-1,y
-                               + cx*u0[row_y:rows][col_x+1:cols]  // x+1,y
-                               + cy*u0[row_y-1:rows][col_x:cols]  // x,y-1
-                               + cy*u0[row_y+1:rows][col_x:cols]; // x,y+1
-
-
-
-    // Reflect Boundaries
-    u1[0][0:SIM_COLS] = u1[1][0:SIM_COLS];
-    u1[SIM_ROWS-1][0:SIM_COLS] = u1[SIM_ROWS-2][0:SIM_COLS];
-    u1[0:SIM_ROWS][0] = u1[0:SIM_ROWS][1];
-    u1[0:SIM_ROWS][SIM_COLS-1] = u1[0:SIM_ROWS][SIM_COLS-2];
-
-
-
-    // TODO: REFLECT BOUNDARIES
-
+    // Update state
     std::swap(u0, u1);
-
-
-    tNext =  __sec_reduce_add(u0[0:SIM_ROWS][0:SIM_COLS]);
-    std::cout << "Timestep: " << ts << "start temp:" << tNow 
-                                    << "new temp:" << tNext << std::endl;
-    tNow = tNext;
+    if (t % 128 == 0) {
+      const double tNow = __sec_reduce_add(u0[1:ny-2][1:nx-2]);
+      std::cout << "ts " << t << " temp: " << tNow << std::endl;
+    }
   }
-  Grid<RowExchangePolicy> g;
-  g.step();
 
-  delete[] u0[0:SIM_ROWS];
-  delete[] u1[0:SIM_ROWS];
-  delete[] u1;
+  // De-allocate space for simulation
+  delete[] u0[0:ny]; 
+  delete[] u1[0:ny]; 
   delete[] u0;
+  delete[] u1;
+  return EXIT_SUCCESS;
 }
-
-// Shifting:
-// std::cout << "\nShifted Retrograde\n";
-// u1[0:9][0:9] = u0[1:9][1:9];
